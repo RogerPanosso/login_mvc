cd8b9fbe0f1fd1590257296a5fca84a3058bd010
Roger Panosso <rogerninopa@gmail.com>
2021-09-21 21:19:07 -0300

    Criando perante camada Publica(Public) da estrutura MVC do projeto em arquivo personalizado script.js function executando ação de cadastro via ajax

--- Public/Assets/js/script.js
+++ Public/Assets/js/script.js
@@ -1,1 +1,48 @@
+//define codigo jquery selecionando no documento(página) id de formulário de cadastro de usuário bloquando sua ação
+$(document).ready(function(){
+  $("#formCadastroUser").on("submit",function(event){
+      event.preventDefault();
+      //seleciona elemento HTML form através do metodo DOM
+      let form = document.querySelector("#formCadastroUser");
+      let nome = $("#nome").val();
+      let email = $("#email").val();
+      let senha = $("#senha").val();
+      let data_nascimento = $("#data_nascimento").val();
 
+      if(nome == "") {
+        window.alert("Por favor informe um nome valido");
+        return false;
+      }
+
+      if(email == "" || email.indexOf("@") == -1) {
+        window.alert("Por favor informe um endereço de e-mail valido");
+        return false;
+      }
+
+      if(data_nascimento == "") {
+        window.alert("Por favor informe uma data de nascimento valida");
+        return false;
+      }
+
+      //realiza requisição interna ajax
+      $.ajax({
+        type:"POST",
+        url:"http://localhost/login_mvc/ajaxcadastrouser/salvarCadastro",
+        dataType:"html",
+        data:{
+          nome:nome,
+          email:email,
+          senha:senha,
+          data_nascimento:data_nascimento
+        },
+        success:function(html) {
+          $("#resultCadastroUser").html(html);
+          $("#resultCadastroUser").css("color", "#006400");
+        },
+        error:function(html) {
+          $("#resultCadastroUser").html(html);
+          $("#resultCadastroUser").css("color", "#FF0000");
+        }
+      });
+  });
+});
